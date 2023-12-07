a2cd20d98dfd84a344b460bb5aa88730e04cd03c
Ashutosh Singh <85793291+Ashutosh9670@users.noreply.github.com>
2023-12-07 08:21:57 +0000

    02_cobersionOperations

---
+++ 01_basics/02_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = false;
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinNumber = Number(score)
+console.log(typeof valueinNumber)
+console.log(valueinNumber)
+
+//let cBoolean = boolean()
+
+
+// score = "" and  we convert score value into nummber so it takes value 0 
+
+//score = "" => valueinNumber = 0;
+//score = "44abs" => valueinNumber = NAN(Not a number)
+//let score; => valueinNumber = NaN;
+
+let value = 6;
+let negValue = -value
+console.log(negValue)
+
+let str1 = "Hello"
+let str2 = " Ashu"
+
+let str3 = str1 + str2;
+console.log(str3)
+console.log(1 +"2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + 3 +"4")
+console.log(1 + 2 + "@" )
