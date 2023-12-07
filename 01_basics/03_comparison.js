01a5a88cdb3ddec91e65bfe862d788cd6d1b59b9
Ashutosh Singh <85793291+Ashutosh9670@users.noreply.github.com>
2023-12-07 09:38:51 +0000

    03_Comparison

---
+++ 01_basics/03_comparison.js
@@ -0,0 +1,11 @@
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 1);
+console.log(null >= 0);
+console.log(null == 1);
+console.log(null < 1);
+
+//Note: In javaScript when we comparing null value with any other value what it does it simply treat null value as a zero and compare with any other number
+
+console.log(undefined >= 0);
