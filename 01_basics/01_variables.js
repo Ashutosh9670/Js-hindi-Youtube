35de6d1466497a5c81be66c21cc13f39d336670a
Ashutosh Singh <85793291+Ashutosh9670@users.noreply.github.com>
2023-12-07 06:43:05 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accoundId = 12345
+let accountName = "Ashutosh Singh"
+var accountEmail = "ashu9670@gmail.com"
+var accountPsswrd = "1234567"
+accountCity = "Uttar Pradesh"
+
+/*
+var prefer not to use due to in issue in block scope or fuunctional scope 
+*/
+
+console.log(accountName);
+console.table([accoundId,accountName,accountEmail, accountPsswrd,accountCity]);
