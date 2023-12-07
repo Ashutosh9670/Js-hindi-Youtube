aeb90cf9bebe2c21e4b4e1b0acc168a3abae3894
Ashutosh Singh <85793291+Ashutosh9670@users.noreply.github.com>
2023-12-07 06:52:59 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,22 @@
+"use strict";//treat all javascript file as a newer version 
+//alert( 3 + 3)// we are using nodejs not a browser so here it throw error if we run this on codespace
+
+console.log(3 + 3)
+
+console.log("Ashutosh") // it makes code more redable 
+
+let name = 'Ashutosh'
+let age = 21
+let isLoggedIn = false;
+
+/*types of data types in java Script
+1.numeber <= range 2 to pwer 53 something
+2.bigint
+3.boolean
+4.string
+5.object
+
+*/
+//Note : null is object type datatype and undefined is in itself datatype undefined 
+
+console.log(typeof null)
