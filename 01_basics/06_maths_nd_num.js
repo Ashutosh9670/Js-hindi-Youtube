c039d4cc1e55ae41c7a072f7e56cdb12eae291ff
Ashutosh Singh <85793291+Ashutosh9670@users.noreply.github.com>
2023-12-08 05:38:26 +0000

    06_maths_nd_num

---
+++ 01_basics/06_maths_nd_num.js
@@ -0,0 +1,15 @@
+const score = 400.12345
+console.log(score)  //here there is no gaurantee is number coming from user
+
+const numb = new Number(100) // here is gaurantee whatever coming from user it should be number
+console.log(score.toString().length)
+console.log(score.toLocaleString(4))
+
+
+
+/* ******************* MATHS ****************** */
+console.log(Math)
+const num = 25;
+console.log(Math.sqrt(num))
+
+console.log(Math.min(1,2, 3, 4, 5, 6))
