811cb675ef5ce3095d02da43a338ad2abab1ebc0
Ashutosh Singh <85793291+Ashutosh9670@users.noreply.github.com>
2023-12-08 04:58:08 +0000

    05_string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,10 @@
+const name = "Ashu"
+const repo = 50
+
+// console.log(name + repo + " value")
+
+console.log(`Hello my name is ${name} and my rep count is ${repo}`)
+
+const newGame = new String('Ashutosh')
+console.log(newGame.anchor)
+console.log(newGame.blink)
