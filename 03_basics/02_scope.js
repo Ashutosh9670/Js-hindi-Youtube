6e67303ba1d309d50ad3578aa558e60a2eb38b2f
Ashutosh Singh <85793291+Ashutosh9670@users.noreply.github.com>
2023-12-10 12:10:12 +0000

    02_scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,11 @@
+// var a = 300
+if(true){
+
+    let a = 30
+    var b = 40
+    const c = 50
+}
+
+// console.log(a)
+console.log(b) //var is printing inside the loop beceause it work a function scope as well as global scope
+// console.log(c)
